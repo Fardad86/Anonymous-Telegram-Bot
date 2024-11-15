5c02576cde93d0afa3b1c3668e80c102ee55f648
Fardad86 <136969613+Fardad86@users.noreply.github.com>
2024-11-15 23:25:14 +0330

    Update BotConfigs.cs

--- HarfeToBeBot v2.0/HarfeToBeBot v2.0/Controller/BotConfigs.cs
+++ HarfeToBeBot v2.0/HarfeToBeBot v2.0/Controller/BotConfigs.cs
@@ -9,7 +9,7 @@ namespace HarfeToBeBot_v2._0.Controller {
     static class BotConfigs {
 
         // Bot Detail
-        public const string BOT_TOKEN = "";
+        public const string BOT_TOKEN = "7727990235:AAGarkAPneLkSMhWSq9wfL7SCcMuyl2S_wE";
         public const string BOT_NAME = "HarfeToBeBot?start=";
         public const string BOT_INSTAGRAM_LINK = "https://t.me/" + BOT_NAME;
         public const string BOT_TELEGRAM_LINK = "https://telegram.me/" + BOT_NAME;
